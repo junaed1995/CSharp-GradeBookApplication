cd0d15f68d8e25b09d59aad18d6420995617eab4
Junaed Siddiquee <junaedsiddiquee@gmail.com>
2023-04-30 19:20:12 +0530

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -36,7 +36,23 @@ namespace GradeBook.GradeBooks
                 return 'F';
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students " +
+                    "with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStatistics();
+        }
 
+        public override void CalculateStudentStatistics(String Name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with " +
+                    "grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStudentStatistics(Name);
+        }
 
     }
 }
